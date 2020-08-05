af8575b4bd01abe1ead242cb8505af74a658b07d
Joe Nahm <joenahm@yeah.net>
2020-08-05 10:12:25 +0100

    添加发布内容

---
+++ build/precache-manifest.56a5f34b635c124f08154a47b8ff6777.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b15cfc9e773dd99da1b9",
+    "url": "/static/css/main.647a23e8.chunk.css"
+  },
+  {
+    "revision": "b15cfc9e773dd99da1b9",
+    "url": "/static/js/main.685abb1f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "377301da8936171b5fd5",
+    "url": "/static/css/2.14a67fe5.chunk.css"
+  },
+  {
+    "revision": "377301da8936171b5fd5",
+    "url": "/static/js/2.03893520.chunk.js"
+  },
+  {
+    "revision": "0011dfd88b9a5ce91c6ee19327db3abc",
+    "url": "/static/media/wechat.0011dfd8.png"
+  },
+  {
+    "revision": "6430919df68bbb8c4ffbddd5abd1e3ad",
+    "url": "/static/media/alipay.6430919d.jpg"
+  },
+  {
+    "revision": "f0760639456c25440aabab5a7ce92745",
+    "url": "/index.html"
+  }
+];
